ee971ea951df7d7c35a54d4a753a0756359701e7
wangtianli02 <wangtianli02@meituan.com>
2019-08-09 01:56:01 +0800

    feat: add login route

---
+++ server/router/login.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const UserSchema = require('../db/mongoose/index');
+
+const login = async function(ctx) {
+    try {
+        const {username, password} = ctx.request.body;
+        const User = mongoose.model('User', UserSchema);
+        const res = await User.find({
+            user_name: username
+        });
+        console.log(res[0], password)
+        if (res[0].password === password) {
+            ctx.response.body = {
+                code: 200,
+                msg: '登陆成功'
+            }
+        } else {
+            ctx.response.body = {
+                code: 400,
+                msg: '登录失败'
+            };
+        }
+    } catch (e) {
+        console.error(e);
+        ctx.response.body = {
+            code: 400,
+            msg: '登录失败'
+        };
+    }
+};
+module.exports.register = function(router, app) {
+    router.post('/login', login);
+};
---
+++ server/router/register.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpackConfig = require('../../webpack.dev.conf.js');
+const mongoose = require('mongoose');
+const UserSchema = require('../db/mongoose/index');
+
+const register = async function(ctx) {
+    try {
+        const {username, password} = ctx.request.body;
+        // const data = JSON.parse(ctx.request.body);
+        const User = mongoose.model('User', UserSchema);
+        const res = await new User({
+            user_name: username,
+            password,
+            isVip: false
+        }).save();
+        ctx.body = {
+            code: 200,
+            msg: '注册成功'
+        }
+    } catch (e) {
+        console.error(e);
+        ctx.body = {
+            code: 400,
+            msg: '注册失败'
+        };
+    }
+};
+module.exports.register = function(router, app) {
+    router.post('/register', register);
+};
